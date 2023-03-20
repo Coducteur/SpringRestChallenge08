a8a6ca86273cd0566b3a962639aae986b18f4a71
Coducteur <chat.adr01@gmail.com>
2023-03-20 23:17:50 +0100

    critères CRUD ok, searchByTerm doesn't work)

---
+++ src/main/java/cours/springRest/controller/BookController.java
@@ -0,0 +1,47 @@
+package cours.springRest.controller;
+
+import cours.springRest.entity.Book;
+import cours.springRest.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class BookController {
+    @Autowired
+    BookRepository bookRepository;
+
+    @GetMapping("/books")
+    public List<Book> getListOfAllBooks(){
+        return bookRepository.findAll();
+    }
+    @GetMapping("/books/{id}")
+    public Book showBook(@PathVariable int id){
+        return bookRepository.findById(id).get();
+    }
+    @PostMapping("/books")
+    public Book createNewBook(@RequestBody Book newbook){
+        return bookRepository.save(newbook);
+    }
+    //@PostMapping("/books/search")
+    //public List<Book> search(@RequestBody Map<String, String> body){
+        //String searchTerm = body.get("text");
+        //return bookRepository.findInTitleOrDescription(searchTerm, searchTerm);
+    //}
+    @PutMapping("/books/{id}")
+    public Book update(@PathVariable int id,@RequestBody Book book){
+        Book bookToUpdate = bookRepository.findById(id).get();
+        bookToUpdate.setTitle(book.getTitle());
+        bookToUpdate.setAuthor(book.getAuthor());
+        bookToUpdate.setDescription(book.getDescription());
+        return bookRepository.save(bookToUpdate);
+    }
+    @DeleteMapping("/books/{id}")
+    public boolean delete(@PathVariable int id){
+        bookRepository.deleteById(id);
+        return true;
+    }
+
+}
---
+++ src/main/java/cours/springRest/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package cours.springRest.repository;
+
+import cours.springRest.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+    //List<Book> findInTitleOrDescription(String text, String textAgain);
+}
